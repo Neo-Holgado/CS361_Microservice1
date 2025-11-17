6dbd7764859c772fa8edc062c71ec1fb7b04fb90
Drew Taylor <121739743+drewtaylor77@users.noreply.github.com>
2025-11-17 14:58:09 -0800

    Update comments

--- Client.py
+++ Client.py
@@ -3,7 +3,7 @@ import zmq
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
-socket.send_string("ChallengeName")
+socket.send_string("Find the treasure")
 message = socket.recv()
 print(f"{message.decode()}")
 socket.send_string("Q")
